f86d5f391e208fc6fd36432ef94fa7fc25d877fe
Nino Akut <akutnino@gmail.com>
2024-02-12 00:01:59 +0800

    added another solution to the reverse a string challenge

--- basic-algo-scripting/2-reverse-a-string.js
+++ basic-algo-scripting/2-reverse-a-string.js
@@ -7,3 +7,14 @@ function reverseString(string) {
 
 const output = reverseString(myString);
 console.log(output);
+
+// Other Solutions...
+function solutionTwo(string) {
+  const charArray = [];
+
+  for (let i = 0; i < [...string].length; i++) {
+    charArray.unshift(string[i]);
+  }
+
+  return charArray.join('');
+}
