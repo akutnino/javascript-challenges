6c3dc7a0373b0b5a3f366caa11a2ca618bed3d07
Nino Akut <akutnino@gmail.com>
2024-04-27 00:47:48 +0800

    reviewed sort by height, and missing letters

--- basic-algo-scripting/basic-algo-scripting-review-1.js
+++ basic-algo-scripting/basic-algo-scripting-review-1.js
@@ -396,8 +396,49 @@ console.log(seekAndDestroyOutput);
 // sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
 
 function sortByHeight(array) {
-	return;
+	const indexArray = [];
+	const numbersArray = [];
+
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] === -1) indexArray.push(i);
+		if (array[i] !== -1) numbersArray.push(array[i]);
+	}
+
+	const sortedArray = Array.from(numbersArray).sort();
+
+	for (let j = 0; j < sortedArray.length; j++) {
+		if (indexArray.includes(j)) sortedArray.splice(j, 0, -1);
+	}
+
+	return sortedArray;
 }
 
 const sortByHeightOutput = sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]);
 console.log(sortByHeightOutput);
+
+// CHALLENGE 16: MISSING LETTERS
+// Find the missing letter in the passed letter range and return it. If all letters are
+// present, return undefined.
+// ex. missingLetters('abce') === 'd'
+// ex. missingLetters('abcdefghjklmno') === 'i'
+// ex. missingLetters('abcdefghijklmnopqrstuvwxyz') === 'undefined'
+
+function missingLetters(string) {
+	const charArray = [...string];
+	const firstChar = string.at(0);
+	const lastChar = string.at(-1);
+	const charOuputArray = [];
+
+	for (let i = firstChar.charCodeAt(); i <= lastChar.charCodeAt(); i++) {
+		charOuputArray.push(String.fromCharCode(i));
+	}
+
+	const outputArray = charOuputArray.filter((char) =>
+		charArray.includes(char) ? false : true
+	);
+
+	return outputArray[0];
+}
+
+const missingLettersOutput = missingLetters('abcdefghjklmno');
+console.log(missingLettersOutput);
