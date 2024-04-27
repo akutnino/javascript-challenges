9ab961f8252735bcdf70f72af6d939197c110679
Nino Akut <akutnino@gmail.com>
2024-04-28 02:22:39 +0800

    reviewed Convert Celsius to Fahrenheit

--- basic-algo-scripting/basic-algo-scripting-review-1.js
+++ basic-algo-scripting/basic-algo-scripting-review-1.js
@@ -1,1 +1,11 @@
-// Answer All Algo Scripting Questions from Chris Cooper YT playlist.
+// Answer All Algo Scripting Questions from Stral Tech's YT playlist.
+
+// Convert Celsius to Fahrenheit
+function convertToF(celsius) {
+	let fahrenheit = Number(celsius * (9 / 5) + 32);
+
+	return fahrenheit;
+}
+
+const convertToFahrenOuput = convertToF(20);
+console.log(convertToFahrenOuput);
