9d796eba0e67b06d534b8fda7754983a2220cef1
Nino Akut <akutnino@gmail.com>
2024-05-01 01:04:06 +0800

    reviewed Sum All Numbers in a Range

--- intermediate-algo-scripting/inter-algo-scripting-review-1.js
+++ intermediate-algo-scripting/inter-algo-scripting-review-1.js
@@ -1,1 +1,19 @@
 // Answer All Algo Scripting Questions from Chris Cooper YT playlist.
+
+// Sum All Numbers in a Range
+function sumAll(array) {
+	const NEEDED_INDEX = 1;
+	const [firstNumber, secondNumber] = array;
+	const smallNumber = firstNumber < secondNumber ? firstNumber : secondNumber;
+	const largeNumber = secondNumber > firstNumber ? secondNumber : firstNumber;
+	const totalValues = largeNumber - smallNumber + NEEDED_INDEX;
+
+	const sumTotal = Array.from(Array(totalValues))
+		.map((val, index) => index + smallNumber)
+		.reduce((acc, curr) => (acc += curr), 0);
+
+	return sumTotal;
+}
+
+const sumAllOutput = sumAll([5, 10]);
+console.log(sumAllOutput);
